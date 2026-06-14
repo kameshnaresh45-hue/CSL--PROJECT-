19ecaabb8d86ad1a899199c553f34334733f404d
kameshnaresh45-hue <kameshnaresh45@gmail.com>
2026-06-14 21:37:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
-from google import genai
+import google.generativeai as genai
+
+# Configure API Key
+genai.configure(api_key="AQ.Ab8RN6ISl3XJ8BPW7X_R3s3sGa-Cbu5kD-sWP9OW0Wvhgnbsjw")
+
 app = Flask(__name__)
-client = genai.Client(api_key="AQ.Ab8RN6LKqXfyQ4ypOooSOOvg8x8rHBYvRcOl0ITa-tK63y-EKg")
+
+# Load Gemini Model
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -9,17 +16,24 @@ def home():
     response_text = ""
 
     role = """
-You are AI Research Knowledge Assistant.,Explain AI, Machine Learning, Deep Learning concepts,Answer research-related questions.
+You are an AI Research Knowledge Assistant.
+Explain AI, Machine Learning, and Deep Learning concepts.
+Answer research-related questions clearly.
 """
 
     if request.method == "POST":
 
         prompt = request.form["prompt"]
-        response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f" your role :{role},user input : {prompt}"
-)
-     
+
+        full_prompt = f"""
+Role:
+{role}
+
+User Input:
+{prompt}
+"""
+
+        response = model.generate_content(full_prompt)
 
         response_text = response.text
 
@@ -27,6 +41,7 @@ You are AI Research Knowledge Assistant.,Explain AI, Machine Learning, Deep Lear
         "index.html",
         message=response_text
     )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
