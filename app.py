87f7d2966729f11f30f45f3f1f198ba1c0163122
kameshnaresh45-hue <kameshnaresh45@gmail.com>
2026-06-14 20:24:50 +0530

    Refactor OpenAI client and app.run configuration
    Removed debug mode from app.run and adjusted OpenAI client initialization.

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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"
-)
+    api_key="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")
 @app.route("/", methods=["GET", "POST"])
 def home():
 
@@ -34,4 +33,4 @@ You are AI Research Knowledge Assistant.,Explain AI, Machine Learning, Deep Lear
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0")
