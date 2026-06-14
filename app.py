d2fa35714452198dba4c515cb1ef4f402d77309f
kameshnaresh45-hue <kameshnaresh45@gmail.com>
2026-06-14 16:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key="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"
-
+)
 @app.route("/", methods=["GET", "POST"])
 def home():
 
