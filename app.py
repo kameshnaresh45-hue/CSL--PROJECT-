e863eb59f9ec3303bf68d191b1971f34e9f855a9
kameshnaresh45-hue <kameshnaresh45@gmail.com>
2026-06-14 20:11:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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"
+    api_key="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"
 )
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -13,13 +13,7 @@ def home():
     response_text = ""
 
     role = """
-You are AI Research Knowledge Assistant.
-
-Responsibilities:
- Explain AI, Machine Learning, Deep Learning concepts,
- Answer research-related questions,
- Summarize research papers,generate project ideas, provide literature review guidance
-
+You are AI Research Knowledge Assistant.,Explain AI, Machine Learning, Deep Learning concepts,Answer research-related questions.
 """
 
     if request.method == "POST":
